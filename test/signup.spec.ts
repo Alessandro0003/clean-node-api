e935faf1d97b3f847874864bea2fd4518dc6a4bc
Alessandro0003 <alessandroquinta3@gmail.com>
2023-12-23 23:38:30 -0300

    :ok_hand: chore: changing path to tests in arquive jest.config

--- test/signup.spec.ts
+++ test/signup.spec.ts
@@ -1,5 +1,6 @@
 import { SignUpController } from '../src/presentation/controllers/signup'
 import { MissingParamError } from '../src/presentation/errors/missing-params-error'
+
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
