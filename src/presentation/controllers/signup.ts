57ea1211dd43cc56554b3129ffbbd07724656507
Alessandro0003 <alessandroquinta3@gmail.com>
2023-12-23 23:33:37 -0300

    :ok_hand: style: code style

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,6 +2,7 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helpers'
 import { Controller } from '../protocols/controller'
+
 export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
