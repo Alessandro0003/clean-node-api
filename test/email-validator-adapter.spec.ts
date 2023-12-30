006257794fefcd98c9ad54af121ed2e8544efe5b
Alessandro0003 <alessandroquinta3@gmail.com>
2023-12-29 23:56:05 -0300

    :test_tube: test: ensure EmailValidatorAdapter calls validator with correct email

--- test/email-validator-adapter.spec.ts
+++ test/email-validator-adapter.spec.ts
@@ -15,9 +15,16 @@ describe('EmailValidator Adapter', () => {
     expect(isValid).toBe(false)
   })
 
-  test('Should return false if validator returns false', () => {
+  test('Should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@email.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 })
