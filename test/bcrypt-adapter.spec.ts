fc6731d1690de72842b0115ab2d08c8a0bc9d9af
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-02 21:51:57 -0300

    :test_tube: test: ensure BcryptAdapter throws if bcrypt throws

--- test/bcrypt-adapter.spec.ts
+++ test/bcrypt-adapter.spec.ts
@@ -25,4 +25,14 @@ describe('Bcrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    jest.spyOn(bcrypt, 'hash').mockImplementation(async () => {
+      return new Promise((resolve, reject) => reject(new Error()))
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
