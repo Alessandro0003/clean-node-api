784958523c591b3fe37ff9002590398a2b9e3600
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-03 00:04:34 -0300

    :heavy_minus_sign: refactor: clear database between tests

--- test/account.spec.ts
+++ test/account.spec.ts
@@ -10,6 +10,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountColletion = MongoHelper.getCollection('accounts')
+    await accountColletion.deleteMany({})
+  })
+
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
