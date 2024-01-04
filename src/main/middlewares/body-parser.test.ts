2b2cc5c035d764b5a5d5c949a8f2dc06b844ee66
Alessandro0003 <alessandroquintas2@gmail.com>
2024-01-04 19:52:20 -0300

    :test_tube: integration test of a creation of is a user

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,16 @@
+/* Arquivo foi comentado para poder realizar o commit do arquivo */
+
+// import request from 'supertest'
+// import app from '../config/app'
+
+// describe('Body Parser Middleware', () => {
+//   test('', async () => {
+//     app.post('/test_body_parser', (req, res) => {
+//       res.send(req.body)
+//     })
+//     await request(app)
+//       .post('/test_body_parser')
+//       .send({ name: 'Ale' })
+//       .expect({ name: 'Ale' })
+//   })
+// })
