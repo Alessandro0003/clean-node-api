27c66d822c3e6139bc29ee45a2d54b7ba0c2f9b1
Alessandro0003 <alessandroquintas2@gmail.com>
2024-01-06 22:49:47 -0300

    :test_tube: test: ensure requests parse body-parse as json

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
-  test('', async () => {
+  test('Should parse body as json', async () => {
     app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
