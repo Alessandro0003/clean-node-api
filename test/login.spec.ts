c314f2b334e920cda7a26c182338c68b6009a65f
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-11 00:40:24 -0300

    :sparkles: test: ensure LoginController returns 400 if no email is provided

---
+++ test/login.spec.ts
@@ -0,0 +1,15 @@
+import { LoginController } from '../src/presentation/controllers/login/login'
+import { MissingParamError } from '../src/presentation/errors'
+import { badRequest } from '../src/presentation/helpers/http-helpers'
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
