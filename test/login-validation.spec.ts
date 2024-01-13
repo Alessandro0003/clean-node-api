a43e87a2fafc456a42512c0eb28ce02f35291792
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-13 01:48:13 -0300

    :truck: refactor: group validation imports in a single file

--- test/login-validation.spec.ts
+++ test/login-validation.spec.ts
@@ -1,8 +1,6 @@
 import { makeLoginValidation } from '../src/main/factories/login/login-validation'
-import { EmailValidation } from '../src/presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '../src/presentation/helpers/validators/required-field-validation'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../src/presentation/helpers/validators'
 import { Validation } from '../src/presentation/protocols/validation'
-import { ValidationComposite } from '../src/presentation/helpers/validators/validation-composite'
 import { EmailValidator } from '../src/presentation/protocols/email-validator'
 
 jest.mock('../src/presentation/helpers/validators/validation-composite')
--- test/signup-validation.spec.ts
+++ test/signup-validation.spec.ts
@@ -1,9 +1,6 @@
 import { makeSignUpValidation } from '../src/main/factories/signup/signup-validation'
-import { CompareFieldsValidation } from '../src/presentation/helpers/validators/compare-fields-validation'
-import { EmailValidation } from '../src/presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '../src/presentation/helpers/validators/required-field-validation'
+import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../src/presentation/helpers/validators'
 import { Validation } from '../src/presentation/protocols/validation'
-import { ValidationComposite } from '../src/presentation/helpers/validators/validation-composite'
 import { EmailValidator } from '../src/presentation/protocols/email-validator'
 
 jest.mock('../src/presentation/helpers/validators/validation-composite')
