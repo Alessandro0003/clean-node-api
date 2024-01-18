968b9d3a913eba70c1d0a6758068c7722c52360c
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-18 01:21:55 -0300

    :test_tube: test: ensure DbAuthentication throws if HashComparer throws

--- test/db-authentication.spec.ts
+++ test/db-authentication.spec.ts
@@ -96,4 +96,14 @@ describe('DbAuthentication UseCase', () => {
 
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
+
+  test('Should throw if HashComparer throws', async () => {
+    const { sut, hashCompareStub } = makeSut()
+
+    jest.spyOn(hashCompareStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const promise = sut.auth(makeFakeAuthentication())
+
+    await expect(promise).rejects.toThrow()
+  })
 })
