9c7f37eb5925b7148deffdc08cb125fc0ea055a2
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-18 16:00:09 -0300

    :recycle: refactor: rename Encrypter to Hasher

--- src/data/use-cases/add-account/db-add-account-protocols.ts
+++ src/data/use-cases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/use-cases/add-account'
-export * from '../../protocols/criptography/encrypter'
+export * from '../../protocols/criptography/hashers'
 export * from '../../protocols/db/add-account-repository'
