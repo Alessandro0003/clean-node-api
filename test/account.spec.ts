c13a53934650180686cb0d9290f1233e426dfb77
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-19 13:33:18 -0300

    :test_tube: test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- test/account.spec.ts
+++ test/account.spec.ts
@@ -15,6 +15,7 @@ describe('Account Mongo Repository', () => {
 
   beforeEach(async () => {
     accountColletion = await MongoHelper.getCollection('accounts')
+
     await accountColletion.deleteMany({})
   })
 
@@ -24,11 +25,13 @@ describe('Account Mongo Repository', () => {
 
   test('Should return an account on add success', async () => {
     const sut = makeSut()
+
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
@@ -38,12 +41,15 @@ describe('Account Mongo Repository', () => {
 
   test('Should return an account on loadByEmail success', async () => {
     const sut = makeSut()
+
     await accountColletion.insertOne({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+
     const account = await sut.loadByEmail('any_email@mail.com')
+
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
@@ -52,7 +58,28 @@ describe('Account Mongo Repository', () => {
   })
   test('Should return null if loadByEmail fails', async () => {
     const sut = makeSut()
+
     const account = await sut.loadByEmail('any_email@mail.com')
+
     expect(account).toBeFalsy()
+  })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const res = await accountColletion.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const fakeAccount = res.ops[0]
+
+    expect(fakeAccount.accessToken).toBeFalsy()
+
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+
+    const account = await accountColletion.findOne({ _id: fakeAccount._id })
+
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
   })
 })
